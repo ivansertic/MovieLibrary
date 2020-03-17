c13279da0fad48f2129efc748a234ac83d04257e
ivansertic <serticivan5@gmail.com>
2020-03-17 18:19:50 +0100

    set index to movies table

--- database/migrations/1583686156965_movie_schema.js
+++ database/migrations/1583686156965_movie_schema.js
@@ -6,11 +6,11 @@ const Schema = use('Schema')
 class MovieSchema extends Schema {
   up () {
     this.create('movies', (table) => {
-      table.increments()
-      table.string('title')
-      table.string('genre')
-      table.string('description')
-      table.integer('length_in_minutes').unsigned()
+      table.increments().index();
+      table.string('title').index();
+      table.string('genre').index();
+      table.string('description');
+      table.integer('length_in_minutes').unsigned().index();
       table.timestamps()
     })
   }
